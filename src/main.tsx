f6a233a55e0ac298f3be8477d6d28d2ea9be1847
cavalinho-xdd <cavalinho-xdd@users.noreply.github.com>
2026-06-17 21:38:39 +0200

    feat: add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import './index.css'
 import './i18n'
 import App from './App.tsx'
 import { PerformanceProvider } from './components/PerformanceContext.tsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <PerformanceProvider>
       <App />
+      <Analytics />
     </PerformanceProvider>
   </StrictMode>,
 )
